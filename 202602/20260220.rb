3d2600df2a5ccf72b90e65ba26f00cbea2522283
わら <fjwr.0401@gmail.com>
2026-02-20 23:59:30 +0900

    practice

--- 202602/20260220.rb
+++ 202602/20260220.rb
@@ -35,3 +35,43 @@ str.each do |s|
   count += 1 if s == "a"
 end
 p count
+
+# 問題
+# 配列から「重複」を取り除いた配列を作ってください。
+#
+# uniq メソッドは禁止。
+#
+# 期待する出力:
+# [1, 2, 3, 4]
+
+numbers = [1, 2, 2, 3, 4, 3, 1]
+
+array =[]
+
+numbers.each do |num|
+  array << num if !array.include?(num)
+end
+
+p array
+
+# 問題
+# 配列の中で「2番目に大きい数」を求めてください。
+#
+# sort / max / uniq 禁止
+#
+# 期待する出力:
+# 12
+
+numbers = [5, 12, 3, 20, 8]
+
+n = numbers.length
+
+(n - 1).times do
+  (n - 1).times do |i|
+    if numbers[i] > numbers[i + 1]
+      numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+    end
+  end
+end
+
+p numbers[n - 2]
